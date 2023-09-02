6406777e4ba20768ff6f48fd53a50240334b19f6
Paul Becerra <paulbecerracs@gmail.com>
2023-09-01 20:23:48 -0500

    cambio de variables

--- pupiletras.cpp
+++ pupiletras.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
+const int row = 5;
+const int col = 6;
+
 void print(char** A) {
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 6; j++) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             std::cout << A[i][j]<<" ";
-            if (j == 5) {
+            if (j == row) {
                 std::cout << std::endl;
             }
         }
@@ -19,7 +22,7 @@ bool palindromo(char* word,int n){
 }
 
 bool findV1(char**A, int i, int j, char* word, int n){
-    if (5-n-i<0 || A[i][j]!=word[0])
+    if (row-n-i<0 || A[i][j]!=word[0])
         return 0;
     else{
         for(int c=1;c<n;c++){
@@ -43,7 +46,7 @@ bool findV2(char**A, int i, int j, char* word, int n){
 }
 
 bool findH1(char**A, int i, int j, char* word, int n){
-    if (6-n-j<0 || A[i][j]!=word[0])
+    if (col-n-j<0 || A[i][j]!=word[0])
         return 0;
     else{
         for(int c=1;c<n;c++){
@@ -69,8 +72,8 @@ bool findH2(char**A, int i, int j, char* word, int n){
 int find(char**A,char* word,int n){
     int res=0;
 
-    for (int i=0;i<5;i++){
-        for (int j=0;j<6;j++){
+    for (int i=0;i<row;i++){
+        for (int j=0;j<col;j++){
             res+=findV1(A,i,j,word,n);
             res+=findV2(A,i,j,word,n);
             res+=findH1(A,i,j,word,n);
@@ -88,15 +91,14 @@ int find(char**A,char* word,int n){
 
 
 int main(){
-    const int row = 5;
-    const int col = 6;
 
     char** A = new char* [row];
     for (char i = 0; i < row; i++)
     {
         A[i] = new char[col];
     }
-    char valores[row][col] = {
+    
+    char pupiletras[row][col] = {
         {'P', 'J', 'P', 'A', 'T', 'O'},
         {'P', 'L', 'O', 'T', 'A', 'P'},
         {'A', 'T', 'T', 'H', 'L', 'O'},
@@ -105,20 +107,20 @@ int main(){
 
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < col; ++j) {
-            A[i][j] = valores[i][j];
+            A[i][j] = pupiletras[i][j];
         }
     }
 
     print(A);
 
-    char s[]="P";
+    char s[]="PATO";
     int n=sizeof(s)-1;
 
     int res=find(A,s,n);
 
     std::cout<<"The word "<<s<<" was found "<<res<<" times.";
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<row;i++){
         delete[] A[i];
     }
     delete[] A;
