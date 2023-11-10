b57f4ca15ed2be9d9b9029dafac0ad757cb82af5
Paul Becerra <paulbecerracs@gmail.com>
2023-11-10 17:40:04 -0500

    finish insert

--- RBtree.cpp
+++ RBtree.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <stack>
-#include <assert.h>
+
 #define y node<T>
 using namespace std;
 
@@ -24,17 +24,17 @@ struct node {
 
 template <class T>
 struct RBtree {
-	y* root = 0;	
+	y* root = 0;
 	bool insert(T v);
 	bool remove(T v);
-	void print();	
+	void print();
 private:
 	void inOrder(y* r);
 	bool find(T v, y**& p, stack<y**>& s);
 	void balance(stack<y**>& s);
 	void changeColor(y* n);
 	void redUncle(y** f, bool leftFather);
-	void blackUncle(y** &f, bool leftChild, bool leftFather);
+	void blackUncle(y**& f, bool leftChild, bool leftFather);
 };
 
 template <class T>
@@ -67,7 +67,7 @@ bool RBtree<T>::insert(T v) {
 		(*p) = new y(v);
 		(*p)->red = 0;
 	}
-		
+
 	balance(s);
 	return 1;
 }
@@ -91,7 +91,7 @@ void RBtree<T>::redUncle(y** f, bool leftFather) {
 }
 
 template <class T>
-void RBtree<T>::blackUncle(y** &f, bool leftChild, bool leftFather) {
+void RBtree<T>::blackUncle(y**& f, bool leftChild, bool leftFather) {
 
 	if (leftChild && leftFather) {//lineleft(/)
 
@@ -131,25 +131,76 @@ void RBtree<T>::blackUncle(y** &f, bool leftChild, bool leftFather) {
 			(*f)->right->left = tmp1;//Cl->Br
 			if (tmp1) {//existe Br?
 				tmp1->up = (*f)->right;//BRu->C
-			}			
+			}
 		}
 		changeColor(*f);
 		changeColor((*f)->right);
 	}
 
 	else if (leftChild && !leftFather) {//righttriangle(>)
-		cout << "1 0";
-
+		y* tmp = (*f)->left->right;//tmp->Br
+		(*f)->left->right = (*f);//Br->C
+		if (tmp) tmp->up = (*f);//Si BRu, BRu->C
+		(*f)->left->up = (*f)->up;//Bu->A
+		(*f) = (*f)->left;//Ar->B
+		(*f)->right->up = (*f);//Cu->B
+		(*f)->right->left = tmp;//Cl->Br
+		blackUncle(f, 0, 0);
 	}
 
 	else if (!leftChild && leftFather) {//lefttriangle(<)
-		cout << "0 1";
-
+		y* tmp = (*f)->right->left;//tmp->Bl
+		(*f)->right->left = (*f);//Bl->A
+		if (tmp) tmp->up = (*f);//Si BLu, BLu->A
+		(*f)->right->up = (*f)->up;//Bu->C
+		(*f) = (*f)->right;//Cr->B
+		(*f)->left->up = (*f);//Au->B
+		(*f)->left->right = tmp;//Ar->Bl
+		blackUncle(f, 1, 1);
 	}
 
-	else {//lineright(\)
-		cout << "0 0";
+	else {//lineright(/)
 
+		if ((*f)->up->up) {//tiene bisabuelo?
+			if ((*f)->up->up->left == (*f)->up) {//el abuelo es hijo izquierdo
+				(*f)->up->up->left = (*f);//r->B
+				y* tmp1 = (*f)->left;//tmp1->Bl
+				(*f)->left = (*f)->up;//Bl->A
+				(*f)->up = (*f)->up->up;//Bu->r
+				(*f)->left->up = *f;//Au->B
+				f = &((*f)->up->left);//f->r
+				(*f)->left->right = tmp1;//Ar->Bl
+				if (tmp1) {//existe Bl?
+					tmp1->up = (*f)->left;//BRu->A
+				}
+			}
+			else {//el abuelo es hijo derecho
+				(*f)->up->up->right = (*f);//r->B
+				y* tmp1 = (*f)->left;//tmp1->Bl
+				(*f)->left = (*f)->up;//Bl->A
+				(*f)->up = (*f)->up->up;//Bu->r
+				(*f)->left->up = *f;//Au->B
+				f = &((*f)->up->right);//f->r
+				(*f)->left->right = tmp1;//Ar->Bl
+				if (tmp1) {//existe Bl?
+					tmp1->up = (*f)->left;//BRu->A
+				}
+			}
+		}
+		else {//no tiene bisabuelo
+			root = (*f);//r->B
+			y* tmp1 = (*f)->left;//tmp1->Bl
+			(*f)->left = (*f)->up;//Bl->A
+			(*f)->up = 0;//Bu->r
+			(*f)->left->up = *f;//Au->B
+			f = &root;//f->r
+			(*f)->left->right = tmp1;//Ar->Bl
+			if (tmp1) {//existe Bl?
+				tmp1->up = (*f)->left;//BRu->A
+			}
+		}
+		changeColor(*f);
+		changeColor((*f)->left);
 	}
 }
 
@@ -159,13 +210,13 @@ void RBtree<T>::balance(stack<y**>& s) {
 		if ((*s.top())->red) {
 			if ((*s.top())->left) {
 				if ((*s.top())->left->red) {
-					if ((*s.top())->up->left == (*s.top())){
+					if ((*s.top())->up->left == (*s.top())) {
 						if ((*s.top())->up->right && (*s.top())->up->right->red)
 							redUncle(s.top(), 1);
 						else
 							blackUncle(s.top(), 1, 1);
 					}
-					else{
+					else {
 						if ((*s.top())->up->left && (*s.top())->up->left->red)
 							redUncle(s.top(), 0);
 						else
@@ -173,19 +224,19 @@ void RBtree<T>::balance(stack<y**>& s) {
 					}
 				}
 			}
-			if ((*s.top())->red  && (*s.top())->right) {
+			if ((*s.top())->red && (*s.top())->right) {
 				if ((*s.top())->right->red) {
-					if ((*s.top())->up->right == (*s.top())){
+					if ((*s.top())->up->right == (*s.top())) {
 						if ((*s.top())->up->left && (*s.top())->up->left->red)
-							redUncle(s.top(), 1);
-						else
-							blackUncle(s.top(), 0, 1);
-					}
-					else{
-						if ((*s.top())->up->left->red)
 							redUncle(s.top(), 0);
 						else
 							blackUncle(s.top(), 0, 0);
+					}
+					else {
+						if ((*s.top())->up->right && (*s.top())->up->right->red)
+							redUncle(s.top(), 1);
+						else
+							blackUncle(s.top(), 0, 1);
 					}
 				}
 			}
@@ -202,7 +253,7 @@ void RBtree<T>::balance(stack<y**>& s) {
 
 template <class T>
 bool RBtree<T>::remove(T v) {
-
+	return 0;
 }
 
 template <class T>
@@ -224,11 +275,17 @@ void RBtree<T>::inOrder(y* r) {
 int main() {
 	RBtree<int> t1;
 	t1.insert(50);
+
 	t1.insert(25);
-	//t1.insert(75);
 	t1.insert(13);
-	//t1.insert(63);
 	t1.insert(1);
+
+	t1.insert(55);
+	t1.insert(60);
+	t1.insert(65);
+
+	t1.insert(80);
+	t1.insert(70);
 
 	t1.print();
 
