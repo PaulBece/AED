234ed0def32f464a64f6e78a1dfb11b4f539f731
Paul Becerra <paulbecerracs@gmail.com>
2023-11-17 12:37:54 -0500

    add remove and change find and insert

--- RBtree.cpp
+++ RBtree.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <queue>
-#include <stack>
 
 #define y node<T>
+#define left nodes[0]
+#define right nodes[1]
 using namespace std;
 
 template <typename T>
 struct node {
 	T valor;
-	y* left = 0;
-	y* right = 0;
+	y* nodes[2] = { 0,0 };
 	y* up;
-	bool red = 1;
+	bool lor;//0 es hijo izquierdo de su padre 1 es hijo derecho de su padre
+	bool red = 1;//1 rojo 0 negro
 	node(T v, y* u = 0) {
 		valor = v;
 		up = u;
@@ -30,25 +30,29 @@ struct RBtree {
 	void print();
 private:
 	void inOrder(y* r);
-	bool find(T v, y**& p, stack<y**>& s);
-	void balance(stack<y**>& s);
+	bool find(T v, y**& p, y**& father);
+	void balance(y**& father);
 	void changeColor(y* n);
 	void redUncle(y** f, bool leftFather);
 	void blackUncle(y**& f, bool leftChild, bool leftFather);
+	y* mini(y* p);
+	void fixRem(y** &p, y* q);
 };
 
 template <class T>
-bool RBtree<T>::find(T v, y**& p, stack<y**>& s) {
+bool RBtree<T>::find(T v, y**& p, y**& father) {
 	p = &root;
+	father = 0;
 	while (*p) {
-		s.push(p);
 		if (v < (*p)->valor) {
+			father = p;
 			p = &((*p)->left);
 		}
 		else {
 			if (v == (*p)->valor) {
 				return 1;
 			}
+			father = p;
 			p = &((*p)->right);
 		}
 	}
@@ -58,17 +62,23 @@ bool RBtree<T>::find(T v, y**& p, stack<y**>& s) {
 template <class T>
 bool RBtree<T>::insert(T v) {
 	y** p;
-	stack<y**> s;
-	if (find(v, p, s)) return 0;
-	if (!s.empty()) {
-		(*p) = new y(v, *s.top());
+	y** father;
+	if (find(v, p, father)) return 0;
+	if (father) {
+		(*p) = new y(v, *father);
+		if ((*p)->up->left == (*p)) {
+			(*p)->lor = 0;
+		}
+		else {
+			(*p)->lor = 1;
+		}
 	}
 	else {
 		(*p) = new y(v);
 		(*p)->red = 0;
 	}
 
-	balance(s);
+	balance(father);
 	return 1;
 }
 
@@ -135,6 +145,10 @@ void RBtree<T>::blackUncle(y**& f, bool leftChild, bool leftFather) {
 		}
 		changeColor(*f);
 		changeColor((*f)->right);
+		(*f)->lor = (*f)->right->lor;
+		(*f)->left->lor = 0;
+		(*f)->right->lor = 1;
+		if ((*f)->right->left) (*f)->right->left->lor = 0;
 	}
 
 	else if (leftChild && !leftFather) {//righttriangle(>)
@@ -201,59 +215,282 @@ void RBtree<T>::blackUncle(y**& f, bool leftChild, bool leftFather) {
 		}
 		changeColor(*f);
 		changeColor((*f)->left);
+		(*f)->lor = (*f)->left->lor;
+		(*f)->left->lor = 0;
+		(*f)->right->lor = 1;
+		if ((*f)->left->right) (*f)->left->right->lor = 1;
 	}
 }
 
 template <class T>
-void RBtree<T>::balance(stack<y**>& s) {
-	while (!s.empty()) {
-		if ((*s.top())->red) {
-			if ((*s.top())->left) {
-				if ((*s.top())->left->red) {
-					if ((*s.top())->up->left == (*s.top())) {
-						if ((*s.top())->up->right && (*s.top())->up->right->red)
-							redUncle(s.top(), 1);
-						else
-							blackUncle(s.top(), 1, 1);
+void RBtree<T>::balance(y** &father) {
+	while (father) {
+		if ((*father)->red) {//padre rojo?
+			if ((*father)->left) {//tiene hijo izquierdo?
+				if ((*father)->left->red) {//hijo rojo?
+					if ((*father)->up->nodes[(!(*father)->lor)] && (*father)->up->nodes[(!(*father)->lor)]->red) {//tio rojo?
+						redUncle(father, (!(*father)->lor));
 					}
-					else {
-						if ((*s.top())->up->left && (*s.top())->up->left->red)
-							redUncle(s.top(), 0);
-						else
-							blackUncle(s.top(), 1, 0);
+					else {//tio negro
+						blackUncle(father, 1, (!(*father)->lor));
 					}
 				}
 			}
-			if ((*s.top())->red && (*s.top())->right) {
-				if ((*s.top())->right->red) {
-					if ((*s.top())->up->right == (*s.top())) {
-						if ((*s.top())->up->left && (*s.top())->up->left->red)
-							redUncle(s.top(), 0);
-						else
-							blackUncle(s.top(), 0, 0);
+			if ((*father)->red && (*father)->right) {//hijo derecho
+				if ((*father)->right->red) {//hijo rojo?
+					if ((*father)->up->nodes[(!(*father)->lor)] && (*father)->up->nodes[(!(*father)->lor)]->red) {//tio rojo?
+						redUncle(father, (!(*father)->lor));
 					}
-					else {
-						if ((*s.top())->up->right && (*s.top())->up->right->red)
-							redUncle(s.top(), 1);
-						else
-							blackUncle(s.top(), 0, 1);
+					else {//tio negro
+						blackUncle(father, 0, (!(*father)->lor));
 					}
 				}
 			}
 		}
 		else
 			return;
-		s.pop();
+		if ((*father)->up && (*father)->up->up)
+			father = &((*father)->up->up->nodes[(*father)->up->lor]);
+		else {
+			father = &root;
+		}
+		root->red = 0;
 	}
-	if (root->red)
-		changeColor(root);
 }
 
+template <class T>
+y* RBtree<T>::mini(y* p) {
+	while (p->left) {
+		p = p->left;
+	}
+	return p;
+}
 
+template <class T>
+void RBtree<T>::fixRem(y** &p, y* q) {
+	if ((*p) && (*p)->red) {
+		(*p)->red = 0;
+	}
+	else {
+		bool dBlack = 1;
+		if (!(*p)) {
+			while (dBlack) {
+				if (q->left) {
+					if (q->left->red) {
+						y** qu = &(q->left);
+						if (q->left->lor) blackUncle(qu, 0, 0);
+						else blackUncle(qu, 1, 1);
+					}
+					else {
+						if (q->left->left && q->left->left->red) {//hijo rojo izquierdo
+							p = &(q->left);
+							if ((*p)->lor) {
+								blackUncle(p, 1, 0);
+								changeColor((*p)->left);
+								//changeColor((*p)->right);
+							}
+							else {
+								blackUncle(p, 1, 1);
+								changeColor((*p));
+								changeColor((*p)->right);
+								//changeColor((*p)->left);
+							}
+							dBlack = 0;
+						}
+						else if (q->left->right && q->left->right->red) {//hijo rojo derecho
+							p = &(q->left);
+							if ((*p)->lor) {
+								blackUncle(p, 0, 0);
+								changeColor((*p));
+								changeColor((*p)->left);
+								//changeColor((*p)->right);
+							}
+							else {
+								blackUncle(p, 0, 1);
+								changeColor((*p)->right);
+								//changeColor((*p)->left);
+							}
+							dBlack = 0;
+						}
+						else {//sin hijos rojos
+							changeColor(q->left);
+							if (q->red) {
+								q->red = 0;
+								dBlack = 0;
+							}
+							else {
+								if (q->up) p = &(q->up->nodes[q->lor]);
+								else p = &root;
+							}
+						}
+					}
+				}
+				else {
+					if (q->right->red) {
+						y** qu = &(q->right);
+						if (q->right->lor) blackUncle(qu, 0, 0);
+						else blackUncle(qu, 1, 1);
+					}
+					else {
+						if (q->right->left && q->right->left->red) {//hijo rojo izquierdo
+							p = &(q->right);
+							if ((*p)->lor) {
+								blackUncle(p, 1, 0);
+								changeColor((*p)->left);
+								changeColor((*p)->right);
+							}
+							else {
+								blackUncle(p, 1, 1);
+								changeColor((*p));
+								changeColor((*p)->right);
+								changeColor((*p)->left);
+							}
+							dBlack = 0;
+						}
+						else if (q->right->right && q->right->right->red) {//hijo rojo derecho
+							p = &(q->right);
+							if ((*p)->lor) {
+								blackUncle(p, 0, 0);
+								changeColor((*p));
+								changeColor((*p)->left);
+								changeColor((*p)->right);
+							}
+							else {
+								blackUncle(p, 0, 1);
+								changeColor((*p)->right);
+								changeColor((*p)->left);
+							}
+							dBlack = 0;
+						}
+						else {//sin hijos rojos
+							changeColor(q->right);
+							if (q->red) {
+								q->red = 0;
+								dBlack = 0;
+							}
+							else {
+								if (q->up) p = &(q->up->nodes[q->lor]);
+								else p = &root;
+							}
+						}
+					}
+				}		
+			}
+		}
+		else {
+			while (dBlack && (*p)->up) {
+				if ((*p)->up->nodes[!((*p)->lor)] && (*p)->up->nodes[!((*p)->lor)]->red) {
+					//hermano rojo
+					y** qu = &((*p)->up->nodes[!((*p)->lor)]);
+					if ((*p)->up->nodes[!((*p)->lor)]->lor) blackUncle(qu, 0, 0);
+					else blackUncle(qu, 1, 1);
+				}
+				else {
+					//hermano negro
+					if ((*p)->up->nodes[!((*p)->lor)]->left && (*p)->up->nodes[!((*p)->lor)]->left->red) {//hijo rojo izquierdo
+						p = &((*p)->up->nodes[!((*p)->lor)]);
+						if ((*p)->lor) {
+							blackUncle(p, 1, 0);
+							changeColor((*p)->left);
+							//changeColor((*p)->right);
+						}
+						else {
+							blackUncle(p, 1, 1);
+							changeColor((*p));
+							changeColor((*p)->right);
+							//changeColor((*p)->left);
+						}
+						dBlack = 0;
+					}
+					else if ((*p)->up->nodes[!((*p)->lor)]->right && (*p)->up->nodes[!((*p)->lor)]->right->red) {//hijo rojo derecho
+						p = &((*p)->up->nodes[!((*p)->lor)]);
+						if ((*p)->lor) {
+							blackUncle(p, 0, 0);
+							changeColor((*p));
+							changeColor((*p)->left);
+							//changeColor((*p)->right);
+						}
+						else {
+							blackUncle(p, 0, 1);
+							changeColor((*p)->right);
+							//changeColor((*p)->left);
+						}
+						dBlack = 0;
+					}
+					else {//sin hijos rojos
+						changeColor((*p)->up->nodes[!((*p)->lor)]);
+						if ((*p)->up->red) {
+							(*p)->up->red = 0;
+							dBlack = 0;
+						}
+						else {
+							if ((*p)->up->up) p = &((*p)->up->up->nodes[(*p)->up->lor]);
+							else p = &root;
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 template <class T>
 bool RBtree<T>::remove(T v) {
-	return 0;
+	y** p = &root;
+	y** father;
+	if (!find(v, p, father)) return 0;
+	bool originalColor = (*p)->red;
+	y* q = (*p)->up;
+	if ((*p)->left) {
+		if ((*p)->right) {
+			//función remover con 2 hijos
+			y* mi = mini((*p)->right);
+			originalColor = mi->red;
+			(*p)->valor = mi->valor;
+			mi->up->nodes[mi->lor] = mi->right;
+			if (mi->right) {
+				mi->right->up = mi->up;
+				mi->right->lor = mi->lor;
+			}
+			p = &(mi->up->left);
+			q = mi->up;
+			delete mi;
+		}
+		else {
+			//función remover con solo hijo izquierdo
+			y* tmp = (*p);
+			if ((*p)->up) {
+				(*p)->left->up = (*p)->up;
+				(*p)->left->lor = (*p)->lor;
+				(*p)->up->nodes[(*p)->lor] = (*p)->left;
+				delete tmp;
+			}
+			else {
+				(*p)->left->up = 0;
+				root = (*p)->left;
+				delete tmp;
+			}
+		}
+	}
+	else {
+		y* tmp = (*p);
+		if ((*p)->up) {
+			if ((*p)->right) {
+				(*p)->right->up = (*p)->up;
+				(*p)->right->lor = (*p)->lor;
+			}
+			(*p)->up->nodes[(*p)->lor] = (*p)->right;
+			delete tmp;
+		}
+		else {
+			if ((*p)->right) {
+				(*p)->right->up = 0;
+			}
+			root = (*p)->right;
+			delete tmp;
+		}
+	}
+	if (!originalColor) fixRem(p,q);
 }
 
 template <class T>
@@ -266,9 +503,9 @@ void RBtree<T>::inOrder(y* r) {
 	if (!r) return;
 	inOrder(r->left);
 	if (r->red)
-		cout << r->valor << "-" << r->getFather() << "-R  ";
+		cout << r->valor << "-" << r->getFather() << "-R-" << r->lor << "  ";
 	else
-		cout << r->valor << "-" << r->getFather() << "-B  ";
+		cout << r->valor << "-" << r->getFather() << "-B-" << r->lor << "  ";
 	inOrder(r->right);
 }
 
@@ -286,6 +523,11 @@ int main() {
 
 	t1.insert(80);
 	t1.insert(70);
+
+	t1.remove(80);
+	t1.remove(55);
+	t1.remove(50);
+	//t1.remove(13);
 
 	t1.print();
 
